a88291dfbc1aaeb2b16900dc7e2e7cb5063615af
Graham Smith <grahamas@gmail.com>
2021-10-21 23:06:09 -0700

    Removed more assumptions about plotted array

--- src/plots.jl
+++ src/plots.jl
@@ -1,13 +1,12 @@
 
 using DataFrames, CairoMakie, AlgebraOfGraphics
 
-function plot_eeg_traces(arr::AbstractArray)
-    # TODO call NamedDimsArray constructor
-    @assert size(arr,2) > size(arr,1) # assume first dim is channel
-    tbl = Tables.table(arr', header=["$i" for i ∈ 1:19])
+function plot_eeg_traces(maybe_arr::AbstractArray)
+    arr = NamedDimsArray{(:channel, :time)}(maybe_arr)
+    tbl = Tables.table(arr', header=["$i" for i ∈ axes(arr, :channel)])
     df = DataFrame(tbl)
     df.time_bin = 1:nrow(df)
-    stacked_df = stack(df, 1:19)
+    stacked_df = stack(df, axes(arr, :channel))
     rename!(stacked_df, :value => :signal, :variable => :channel)
     axis = (height=60, width=800)
     aog_data = data(stacked_df)
