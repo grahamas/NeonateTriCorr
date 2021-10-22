ebee038da8a6a3e1b4b85c72d6e08d0fbff10026
Graham Smith <grahamas@gmail.com>
2021-10-21 23:08:11 -0700

    Removing AxisIndices dep

--- src/load.jl
+++ src/load.jl
@@ -1,4 +1,4 @@
-using MAT, AxisIndices
+using MAT, NamedDims
 
 function load_EEG_snippet(filename)
     test_data_vars = matread(datadir("exp_raw", "patient1_sz1_10secs.mat"))
@@ -7,6 +7,8 @@ function load_EEG_snippet(filename)
     @assert Set(keys(test_data_vars)) == expected_vars
 
     eeg_snippet_arr = test_data_vars["snippetSave"]
-    return NamedAxisArray{(:channel,:time_bin)}(eeg_snippet_arr)
+    return NamedDimsArray{(:channel,:time)}(eeg_snippet_arr)
 end
 
+# From TimeAxes
+is_time(sym::Symbol) = sym == :time
