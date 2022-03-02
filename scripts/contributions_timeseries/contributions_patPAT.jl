26c0e1163ac2b44d7188f898a7593d649ff1a873
Graham Smith <grahamas@gmail.com>
2022-03-01 18:05:23 -0700

    seriously stop snipping

--- scripts/contributions_timeseries/contributions_patPAT.jl
+++ scripts/contributions_timeseries/contributions_patPAT.jl
@@ -18,7 +18,7 @@ rms(xs) = sqrt(mean(xs .^ 2))
 
 moving_average(vs, n) = [mean(skipmissing(@view vs[(i-n+1):i])) for i in n:length(vs)]
 
-let eeg = load_helsinki_eeg(PAT), eeg = snip(eeg, 61);
+let eeg = load_helsinki_eeg(PAT);
 
 contributions = calc_class_contributions(eeg, Periodic(), AN_01norm;
         λ_max = (8,25),
