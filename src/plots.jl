f154c6293ff2a85959db6814fa3015fe4d7018d8
Graham Smith <grahamas@gmail.com>
2022-06-04 15:07:05 -0500

    plot_reviewer_consensus!

--- src/plots.jl
+++ src/plots.jl
@@ -1,4 +1,5 @@
 
+using TriCorrApplications
 function TriCorrApplications.plot_reviewer_consensus!(ax, eeg::AbstractProcessedEEG)
     lines!(ax, get_times(eeg, sample_rate=1), eeg.seizure_reviewers_count)
     tightlimits!(ax); hidespines!(ax)
