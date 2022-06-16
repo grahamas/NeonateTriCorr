639f89025e56cc42edec6172f16f3221f38b0d45
Graham Smith <grahamas@gmail.com>
2022-06-16 09:52:57 -0500

    adapt patPAT to preproc

--- scripts/contributions_timeseries/contributions_patPAT.jl
+++ scripts/contributions_timeseries/contributions_patPAT.jl
@@ -14,19 +14,24 @@ include(scriptsdir("include_src.jl"))
 rms(xs) = sqrt(mean(xs .^ 2))
 moving_average(vs, n) = [mean(@view vs[(i-n+1):i]) for i in n:length(vs)]
 
-let eeg = load_helsinki_eeg(PAT),
+PAT = 1
+
+contributions_PAT = let eeg = load_helsinki_eeg(PAT),# eeg = snip(eeg, 600, 975),
     snippets_duration=1, preproc! = zscore!, postproc! = zscore!,
-    assumption = IndStdNormal, conditioned_on = Rate(),
+    assumption = IndStdNormal(), conditioned_on = None(),
     lag_extents = (8,25), plot_traces=true;
 
 unique_id = if @isdefined(parent_session_id)
     parent_session_id
 else
-    session_name = Dates.format(Dates.now(), "yyyy_mm_dd-HHMMSS")
+    Dates.format(Dates.now(), "yyyy_mm_dd-HHMMSS")
 end
 session_name = "tricorr_ts_$(fn2str(preproc!))_$(fn2str(postproc!))_$(obj2str(assumption))_$(obj2str(conditioned_on))_snippets$(snippets_duration)_lagextents$(lag_extents[1])x$(lag_extents[2])_helsinkiEEG$(PAT)_$(unique_id)"
 @show session_name
 
+if preproc! == zscore!
+    preproc! = (o,i) -> zscore!(o,i,mean(i),std(i))
+end
 contributions = calc_class_contributions(eeg, Periodic(), 
         preproc!, postproc!,
         assumption, conditioned_on
@@ -37,6 +42,8 @@ contributions = calc_class_contributions(eeg, Periodic(),
     )
 
 save(datadir("exp_pro", "$(session_name).jld2"), Dict("contributions" => contributions))
+
+# contributions = load(datadir("exp_pro", "$(session_name).jld2"))["contributions"]
 
 if plot_traces
     plots_subdir = plotsdir(session_name)
@@ -61,6 +68,8 @@ end
 # save(joinpath(plots_subdir, "pat$(PAT)_eeg_traces.png"), eeg_fig)
 
 # (rms_fig, conts_fig)
+
+contributions
 
 end
 
