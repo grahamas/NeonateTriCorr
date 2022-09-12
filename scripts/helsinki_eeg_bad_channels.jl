57788c9ddedc89a4077987a15508466f133e7c37
Graham Smith <grahamas@gmail.com>
2022-09-12 10:46:13 -0500

    Standardize bad channels

--- scripts/helsinki_eeg_bad_channels.jl
+++ scripts/helsinki_eeg_bad_channels.jl
@@ -1,13 +1,14 @@
 using DataStructures
 
-helsinki_eeg_bad_channels = DefaultDict((key) -> (@warn "Bad channels undefined for patient $key"; ["ECG","Resp","Cz","Fz"]), Dict(
-    9 => ["ECG","Resp","Cz"],
-    19 => ["ECG","Resp","Cz","Fz"], #unverified
-    21 => ["ECG","Resp","Cz","Fz"], #unverified
-    31 => ["ECG","Resp","Cz"],
-    44 => ["ECG","Resp","Cz"],
-    47 => ["ECG","Resp","Cz","Fz"],
-    50 => ["ECG","Resp","Cz"],
-    62 => ["ECG","Resp","Cz","Fz"], #unverified
-    75 => ["ECG","Resp","Cz","Fz"]
+# Use most conservative bad channels
+helsinki_eeg_bad_channels = DefaultDict((key) -> (["ECG","Resp","Cz","Fz"]), Dict(
+    # 9 => ["ECG","Resp","Cz"],
+    # 19 => ["ECG","Resp","Cz","Fz"], #unverified
+    # 21 => ["ECG","Resp","Cz","Fz"], #unverified
+    # 31 => ["ECG","Resp","Cz"],
+    # 44 => ["ECG","Resp","Cz"],
+    # 47 => ["ECG","Resp","Cz","Fz"],
+    # 50 => ["ECG","Resp","Cz"],
+    # 62 => ["ECG","Resp","Cz","Fz"], #unverified
+    # 75 => ["ECG","Resp","Cz","Fz"]
 ); passkey=true)
